4d3935462f5f11641c6a4eaeb7dbc7e1ada262c5
Ashley Cutmore <ashley.cutmore@gmail.com>
2017-10-02 21:15:19 +0100

    [feature] createImports script
    given the set of type locations and a consuming file will output the required type import statements for each input type

---
+++ createImports.js
@@ -0,0 +1,37 @@
+"use strict";
+/**
+ * createImports.ts
+ * when called with the path to a typescript file,
+ * and the path to a json file listing where types are
+ * will print the required import statements for each type passed to stdin
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+/// <reference types="node" />
+const Path = require("path");
+const ReadLine = require("readline");
+const filePath = Path.dirname(Path.resolve(process.cwd(), process.argv[2]));
+const typeLocationsPath = process.argv[3];
+const typeLocationsDir = Path.dirname(typeLocationsPath);
+const typeLocations = require(typeLocationsPath);
+Object.keys(typeLocations).forEach(key => {
+    const typeLocation = typeLocations[key];
+    const fullPath = Path.resolve(typeLocation, typeLocationsDir);
+    typeLocations[key] = fullPath;
+});
+function resolve(type) {
+    const location = typeLocations[type];
+    if (location == null) {
+        return undefined;
+    }
+    const relativePath = Path.relative(filePath, location);
+    return `import ${type} from ${relativePath}`;
+}
+const rl = ReadLine.createInterface({
+    input: process.stdin,
+});
+rl.on('line', (type) => {
+    const s = resolve(type);
+    if (s !== undefined) {
+        console.log(s);
+    }
+});
---
+++ createImports.ts
@@ -0,0 +1,43 @@
+/**
+ * createImports.ts
+ * when called with the path to a typescript file,
+ * and the path to a json file listing where types are
+ * will print the required import statements for each type passed to stdin
+ */
+
+/// <reference types="node" />
+
+import * as Path from 'path';
+import * as ReadLine from 'readline';
+
+const filePath = Path.dirname(Path.resolve(process.cwd(),  process.argv[2]));
+const typeLocationsPath = process.argv[3];
+const typeLocationsDir = Path.dirname(typeLocationsPath);
+const typeLocations: { [type: string]: string } = require(typeLocationsPath);
+
+Object.keys(typeLocations).forEach(key => {
+  const typeLocation = typeLocations[key];
+  const fullPath = Path.resolve(typeLocation, typeLocationsDir);
+  typeLocations[key] = fullPath;
+});
+
+function resolve(type: string): string | undefined {
+  const location = typeLocations[type];
+  if (location == null) {
+    return undefined;
+  }
+
+  const relativePath = Path.relative(filePath, location);
+  return `import ${type} from ${relativePath}`;
+}
+
+const rl = ReadLine.createInterface({
+  input: process.stdin,
+});
+
+rl.on('line', (type: string) => {
+  const s = resolve(type);
+  if (s !== undefined) {
+    console.log(s)
+  }
+});
